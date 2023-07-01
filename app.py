b47cccac35e23ad5563baa71eec26394e1b8906f
Dhruv Ojha <81473443+Dhruv-Ojha@users.noreply.github.com>
2023-07-02 01:50:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,17 +4,18 @@ import streamlit as st
 import plotly.express as px
 import matplotlib.pyplot as plt
 import seaborn as sns
+from pathlib import Path
 
 st.title("TELECOM CUSTOMER CHURN ANALYSIS")
 st.sidebar.title("TELECOM CUSTOMER CHURN ANALYSIS")
 
 st.sidebar.markdown("This app is a streamlit dashboard to analyze the customer churn data.")
 
-DATA_URL = ("C:\\Users\\Dhruv\\Documents\\ML and Data Analysis Projects\\Telecom Customer Churn Prediction\\preprocessed_data.csv")
+file_path = Path("C:\\Users\\Dhruv\\Documents\\ML and Data Analysis Projects\\Telecom Customer Churn Prediction\\preprocessed_data.csv")
 
 @st.cache_data(persist=True)
 def load_data():
-    data = pd.read_csv(DATA_URL)
+    data = pd.read_csv(file_path)
     return data
 
 data = load_data()
